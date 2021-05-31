e316d3e4adf81a2213fa0a933e810f5daf5f8002
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-05-31 08:28:38 +0700

    refactor: add boxes using custom colors for background

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -25,6 +25,11 @@ class MainActivity : AppCompatActivity() {
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
 
+            // Boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+
             // Set background color to light gray when background is touched
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
