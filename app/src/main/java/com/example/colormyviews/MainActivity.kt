3f97d7c67e4e65bb6fc80d01b272f4472f28fa18
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-05-31 07:24:04 +0700

    refactor: make a call to the setListeners function

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -10,6 +10,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Make a call to the setListeners function
+        setListeners()
     }
 
     private fun makeColored(view: View) {
