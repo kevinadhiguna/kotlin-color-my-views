c5a340741f51429b3207dbc4f10b3774530e6aae
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-05-31 08:30:02 +0700

    refactor: update

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -16,25 +16,9 @@ class MainActivity : AppCompatActivity() {
         setListeners()
     }
 
-    private fun makeColored(view: View) {
-        when (view.id) {
-            // Boxes using Color class colors for the background
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
-            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
-            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
-
-            // Boxes using custom colors for background
-            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
-            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
-            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
-
-            // Set background color to light gray when background is touched
-            else -> view.setBackgroundColor(Color.LTGRAY)
-        }
-    }
-
+    /**
+     * Attaches listeners to all the views.
+     */
     private fun setListeners() {
         // Get a reference for each TextView
         val boxOneText = findViewById<TextView>(R.id.box_one_text)
@@ -56,6 +40,30 @@ class MainActivity : AppCompatActivity() {
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    /**
+     * Sets the background color of a view depending on it's resource id.
+     * This is a way of using one click handler to do similar operations on a
+     * group of views.
+     */
+    private fun makeColored(view: View) {
+        when (view.id) {
+            // Boxes using Color class colors for the background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+
+            // Set background color to light gray when background is touched
+            else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
 }
